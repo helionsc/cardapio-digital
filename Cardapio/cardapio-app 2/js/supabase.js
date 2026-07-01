c189fdf63839497b1e050b88de7769cba65ecd40
Helio Neto Simões Carneiro <56117236+helionsc@users.noreply.github.com>
2026-07-01 20:34:51 -0300

    Change Supabase connection details
    Updated Supabase URL and anon key placeholders.

--- Cardapio/cardapio-app 2/js/supabase.js
+++ Cardapio/cardapio-app 2/js/supabase.js
@@ -1,7 +1,10 @@
-const SUPABASE_URL = "SUA_PROJECT_URL";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY";
+const SUPABASE_URL = "COLE_AQUI_SEU_PROJECT_URL";
+
+const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_ANON_KEY";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+console.log("✅ Supabase conectado");
